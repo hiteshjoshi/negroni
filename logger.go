b22de618d74f4b498625aad956191483343ed628
Hitesh <me@hiteshjoshi.com>
2016-12-31 17:26:34 +0530

    more options

--- logger.go
+++ logger.go
@@ -15,6 +15,7 @@ import (
 // LoggerEntry is the structure
 // passed to the template.
 type LoggerEntry struct {
+	AppName   string
 	StartTime string
 	Status    int
 	Duration  time.Duration
@@ -25,7 +26,7 @@ type LoggerEntry struct {
 
 // LoggerDefaultFormat is the format
 // logged used by the default Logger instance.
-var LoggerDefaultFormat = "{{.StartTime}} | {{.Status}} | \t {{.Duration}} | {{.Hostname}} | {{.Method}} {{.Path}} \n"
+var LoggerDefaultFormat = "{{.AppName}} > {{.StartTime}} | {{.Status}} | \t {{.Duration}} | {{.Hostname}} | {{.Method}} {{.Path}} \n"
 
 // LoggerDefaultDateFormat is the
 // format used for date by the
@@ -40,6 +41,7 @@ type ALogger interface {
 
 // Logger is a middleware handler that logs the request as it goes in and the response as it goes out.
 type Logger struct {
+	AppName string
 	// ALogger implements just enough log.Logger interface to be compatible with other implementations
 	ALogger
 	dateFormat string
@@ -54,7 +56,7 @@ func NewLogger() *Logger {
 }
 
 func (l *Logger) SetAppName(name string) {
-	l.ALogger = log.New(os.Stdout, name+" ", 0)
+	l.AppName = name
 }
 func (l *Logger) SetFormat(format string) {
 	l.template = template.Must(template.New("negroni_parser").Parse(format))
@@ -71,6 +73,7 @@ func (l *Logger) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.Ha
 
 	res := rw.(ResponseWriter)
 	log := LoggerEntry{
+		AppName:   l.AppName,
 		StartTime: start.Format(l.dateFormat),
 		Status:    res.Status(),
 		Duration:  time.Since(start),
